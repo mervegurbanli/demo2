7ed2b6245c8ca9130a92f1caaf5f498fcd8619e1
Merve Gurleyen <mervegurleyeen@gmail.com>
2024-07-05 17:23:48 -0700

    demo

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println(occurenceChar("eeeacrrrrrrr",'e'));
+
+    }
+
+
+    public static int occurenceChar(String str, char c){
+
+        int count = 0;
+
+        for (int i=0; i<str.length(); i++){
+            if(str.charAt(i)==c){
+                count++;
+            }
+        }
+        return count;
+    }
+}
